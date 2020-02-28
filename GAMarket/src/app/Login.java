174a2beb5105280a0f5d00bd3dc62fba4ea4c177
David Serrano <davidserrano@Davids-MacBook-Pro-2.local>
2020-02-28 14:41:09 -0800

    updated code comments

--- GAMarket/src/app/Login.java
+++ GAMarket/src/app/Login.java
@@ -7,24 +7,27 @@ public class Login {
     private String password;
 
     void setUsername(String user){
+
         username = user;
     }
 
     void setPassword(String user){
+
         username = user;
     }
 
     void createNewUser(String username, String password){
-        //create new
+        //to be implemented. method used for a new user registering
     }
 
     boolean checkLogin(String username, String password){
-        //go into database to check whether username & password are correct
-        return false;
+        //to be implemented. user server authentication method to verify credentials
+        return false; //code stub
     }
 
     void loginRecovery(){
-        //some how assist the user to recover their account, preferably through email verification
+        //to be implemented. assist the user with recovering their account, preferably through email verification
+        //but security questions is easier implementation
     }
 
 }
