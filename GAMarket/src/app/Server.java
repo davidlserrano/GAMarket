b8739cf9439f2b950d4acceeef6abeafa5cc8d4c
David Serrano <davidserrano@Davids-MacBook-Pro-2.local>
2020-02-28 14:35:13 -0800

    updated method names & comments

--- GAMarket/src/app/Server.java
+++ GAMarket/src/app/Server.java
@@ -3,12 +3,19 @@ package sample.GAMarket.src.app;
 public class Server {
     int usersOnline;
 
-    boolean authenticateUser(){
-        //check database to see if
-        return false;
+    boolean authenticateUser(String username, String password){
+        //this function should check database(simulated with text file to see if credentials are correct
+        return false; // code stub, remove
     }
-    boolean verifyUserLogin(){
-        return false;
+
+    boolean verifyUserLoggedOn(String username){
+        //this function will check if a 'friend' user is online
+        return false; //code stub, remove
+    }
+
+    String getClientFeed(){
+        //to do
+        return "stub"; //code stub, remove
     }
 
 }
